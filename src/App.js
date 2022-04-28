6596842b1b37e118f50aebda27bf3732ce031400
Gary Flugel <flugelg@gmail.com>
2022-04-28 03:53:10 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div>
       <Header/>
       <TaskList/>
-      <TaskItem/>
     </div>
 
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
 const TaskList = () => {
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/list')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, []);
+
+
   return (
     <div>TaskList</div>
   )
