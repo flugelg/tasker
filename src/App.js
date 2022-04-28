3de2f6c3604087207eed14684f685c171d4e21a1
Gary Flugel <flugelg@gmail.com>
2022-04-28 04:00:45 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
-import TaskItem from './components/TaskItem';
 
 function App() {
   return (
