8648791bc98df075cc76c30d2a698cc0bcea910b
Gary Flugel <flugelg@gmail.com>
2022-04-28 05:40:50 -0400

    changes state to what in inputed

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CreateTask = () => {
+    const [addTodo, setAddTodo] = useState({
+        id: "",
+        tasks: "",
+        due: "",
+        complete: false
+    });
+
+    const handleChange = (event) => {
+        setAddTodo({ ...addTodo, [event.target.name]: event.target.value });
+    };
+
   return (
     <form id="formStyle">
         {/* create an input element */}
-          <input type="text" name="tasks" placeholder="task to add"/>
-          <input type="text" name="due" placeholder="due date of task"/>
+          <input type="text" name="tasks" placeholder="task to add" onChange={handleChange}/>
+          <input type="text" name="due" placeholder="due date of task" onChange={handleChange}/>
           <button id='add-button'>Add Task</button>
     </form>
   )
