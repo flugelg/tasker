fcf9135b943efba567cc6e98c4cbc3d9e906c3bd
Gary Flugel <flugelg@gmail.com>
2022-04-28 03:56:54 -0400

    add table for todo items

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,35 @@
 import React, {useEffect, useState} from 'react';
+import TaskItem from './TaskItem';
 
 const TaskList = () => {
+  const[todoList, setTodoList] = useState([]);
 
   useEffect(() =>{
     fetch('http://localhost:3000/list')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setTodoList(data))
   }, []);
+
+  const displayTodo = todoList.map((todo) =>{
+    return <TaskItem key={todo.id} todo={todo}/>
+  })
 
 
   return (
-    <div>TaskList</div>
+    <table className="task-table">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Due Date</th>
+          <th scope="col">Complete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          displayTodo
+        }
+      </tbody>
+    </table>
   )
 }
 
