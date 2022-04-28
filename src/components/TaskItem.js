38ccaf09799db56481a5ac6aa0530a427053ed7b
Gary Flugel <flugelg@gmail.com>
2022-04-28 03:59:04 -0400

    add items to table

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const TaskItem = () => {
+const TaskItem = ({todo}) => {
+  const {tasks, due, complete} = todo;
+
   return (
-    <div>Item</div>
+    <tr>
+      <td id="task" className={todo.complete ? "strike" : ""}>{tasks}</td>
+      <td id="due" className={todo.complete ? "strike" : ""}>{due}</td>
+      <td>
+        <input id="completeCheck" type="checkbox"/>
+      </td>
+      <td>
+        <button className="btn btn-delete">
+          <span className="mdi mdi-delete mdi-24px"></span>
+          <span className="mdi mdi-delete-empty mdi-24px"></span>
+          <span>Delete</span>
+        </button>
+      </td>
+    </tr>
   )
 }
 
