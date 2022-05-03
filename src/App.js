bb8b339ff76bec851f1f9bfb2db15795017d0ba2
Gary Flugel <flugelg@gmail.com>
2022-05-03 01:57:25 -0400

    Adding routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import { Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import TaskList from './components/TaskList';
+import About from './components/About';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
   return (
     <div>
-      <Route path="/head">
-        <Header/>
-      </Route>
-      <Route path="/">
-        <TaskList/>
-      </Route>
+      <Switch>
+        <Route path="/welcome">
+          <WelcomePage/>
+        </Route>
+        <Route path="/head">
+          <Header/>
+        </Route>
+        <Route path="/">
+          <TaskList/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+      </Switch>
     </div>
 
   );
