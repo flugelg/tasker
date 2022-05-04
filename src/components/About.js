bc43fc3f4bbd658a47dc2c7a6253c8c5689cc9b6
Gary Flugel <flugelg@gmail.com>
2022-05-04 13:03:21 -0400

    Updated about page contents

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div id='about-container'>
+        <div>Do you ever:</div>
+        <ul>
+            <li>You sometimes forget to do things that are important</li>
+            <li>You find it a struggle to keep to deadlines</li>
+        </ul> 
+        <div>It’s a list of tasks you need to complete or things that you want to do.</div>
+    
+    </div>
   )
 }
 
